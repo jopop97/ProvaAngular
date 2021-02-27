9a7b85def566a29953216631fb0ab85810a51478
Giuseppe <giuseppe.lillo97.gl.gl@gmail.com>
2021-02-27 11:24:34 +0100

    CopiaFile
    Copia senza modifiche di tutti i file di my-app

---
+++ my-app/src/items.ts
@@ -0,0 +1,8 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [
+    { nome: 'IPhoneXL', descrizione: 'A large phone with one of the best screens'},
+    { nome: 'Samsung S20', descrizione: 'A large phone with one of the best screens'},
+    { nome: 'Huawei P40 PRO', descrizione: 'A large phone with one of the best screens'},
+    { nome: 'Xiaomi Note10 Ultra', descrizione: 'A large phone with one of the best screens'} 
+];
