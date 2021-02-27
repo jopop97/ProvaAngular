0f027fc45467f9e65a9891d5255354844e76f8b1
Giuseppe <giuseppe.lillo97.gl.gl@gmail.com>
2021-02-27 12:28:31 +0100

    Aggiornamento Finale
    Ora sono esattamente al punto voluto, d'ora in poi posso tornare a lavorare liberamente tra fisso e portatile

--- mia-app/src/app/app.module.ts
+++ mia-app/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppTopBar } from './top/app-top-bar.component';
+import { AppList } from './list/app-product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTopBar,
+    AppList
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,AppList]
 })
 export class AppModule { }
