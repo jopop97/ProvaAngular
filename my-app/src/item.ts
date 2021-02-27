ba11a1d9ae412bb517866a5d4d118fc6f5c36913
Giuseppe <giuseppe.lillo97.gl.gl@gmail.com>
2021-02-27 12:11:49 +0100

    Eliminazione totale
    I file copiati in quel modo non sono funzionanti

--- my-app/src/item.ts
+++
@@ -1,5 +0,0 @@
-export class Item {    
-    nome: string = "";
-    descrizione: string = "";
-
-}
--- my-app/src/items.ts
+++
@@ -1,8 +0,0 @@
-import { Item } from './item';
-
-export const ITEMS: Item[] = [
-    { nome: 'IPhoneXL', descrizione: 'A large phone with one of the best screens'},
-    { nome: 'Samsung S20', descrizione: 'A large phone with one of the best screens'},
-    { nome: 'Huawei P40 PRO', descrizione: 'A large phone with one of the best screens'},
-    { nome: 'Xiaomi Note10 Ultra', descrizione: 'A large phone with one of the best screens'} 
-];
